b20f4906dd1ad7590452127c961284c15e15c320
Sakil Luhar <Luharsakil0@gmail.com>
2023-01-16 10:22:37 +0530

    16/01/23

--- src/Componenets/TopNewsSection.js
+++ src/Componenets/TopNewsSection.js
@@ -28,13 +28,12 @@ function TopNewsSection() {
           <div id='top-news-card' className="card" onClick={tnshandleReadMore}>
             <img id='tns-news-image' src={tech1_jpg} className="card-img" alt="..." />
             <div id='tns-text' className="card-img-overlay">
-<<<<<<< HEAD
             <button id='btnTnsCatagory' className='btn' type="button">Technology</button>
             <h6 id='tns-Title' className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</h6>
-=======
+
             <button id='btnTnsCatagory' className='btn' type="button">Technology</button>
             <h6 id='tns-Title' className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</h6>
->>>>>>> ee8717307736c1aef844091318c75ab0581c1e3d
+
             </div>
           </div>
 
